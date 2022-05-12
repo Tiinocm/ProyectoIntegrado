5a204ef2f6d65bde3042122d106e80086c9a5e1b
Tiinocm <97448098+Tiinocm@users.noreply.github.com>
2022-05-12 17:59:07 +0200

    update consultas

--- consulata para cronos.sql
+++ consulata para cronos.sql
@@ -1,15 +1,23 @@
 use cronos;
-select publicada  from noticias where publicada=1;
-select publicada  from noticias where publicada=0;
-select count(distinct id_noticia ) from noticias;
-select id_noticias  from noticias;
+#1
+select * from noticias where publicada=1;
+#2
+select *  from noticias where publicada=0;
+#3
+select count(id_noticia) from noticias WHERE usuario = "";
+#4
+select *  from noticias where usuario = "";
+#5
 select * from noticias  where id_comunidad=1;
+#6
 select * from comunidad;
+#7
 select * from noticias where publicada=1 and titulo like '%%';
-select * from noticias  where id_comunidad=1 and titulo like '%%';
+#8
+select * from comunidad  where nombre like '%%';
 select * from usuario;
-select * from comunidad where usuario;
-select * from  usuario inner join comunidad where id_comunidad;
+select * from comunidad where usuario = "";
+select usuario.* from  usuario inner join comunidad where id_comunidad = "";
 #insert into  noticias(id_noticias ,id_comunidad,usuario,fecha,votos,imagen_portada,publicada,titulo,posiciones);
 #delete from noticias where id_noticias=valor
 #insert into comunidad(id_comunidad,fecha_creacion,usuario)
