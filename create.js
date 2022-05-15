bc197e33b8c2cb7d8bd938a70ec74360166641df
PC-TINO\Tino <tinoclase@gmail.com>
2022-05-15 13:13:18 +0200

    elementos interactivos del creador de noticias terminado

--- create.js
+++ create.js
@@ -2,16 +2,20 @@ document.addEventListener("DOMContentLoaded", main)
 
 function main() {
     console.log("DOM Content Loaded");
-
+    let contElement = 0;
     let element = document.getElementById("element");
     let add = document.getElementById("add");
 
     add.addEventListener("click", function(e){
         e.preventDefault();
         if (element.value == 0) {
-            console.log("texto");
+            let str = '<textarea name="parrafo" id="text' + contElement + '" cols="50" rows="10"></textarea><br>';
+            document.getElementById("elementNoticia").innerHTML += str;
+            contElement++
         }else if (element.value == 1) {
-            console.log("multimedia");
+            let str = '<input type="file" name="multimedia" id="multimedia' + contElement + '"><br>';
+            document.getElementById("elementNoticia").innerHTML += str;
+            contElement++
         }
     });
 }
