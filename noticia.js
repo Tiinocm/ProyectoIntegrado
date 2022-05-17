5a3a17e174663e95845a6ab02aa32ac45e5e1564
PC-TINO\Tino <tinoclase@gmail.com>
2022-05-17 20:56:22 +0200

    prueba nombre de usuario

--- noticia.js
+++ noticia.js
@@ -8,9 +8,10 @@ function main()
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.responseText);
             console.log(data);
+            console.log(data[0]["usuario"]);
             document.getElementById("dia").innerHTML = data[0]["fecha"];
             document.getElementById("titulo").innerHTML = data[0]["titulo"];
-            document.getElementById("user").innerHTML = data[0]["usuario"];
+/*             document.getElementById("user").innerHTML = data[0]["usuario"]; */
         }
     });
     xhttp.open("GET", "noticia.php" + get, true);
