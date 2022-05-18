e6314cbc7ead1bf269c1bcf5b6b3485b3d195fd9
ALUMNADO\cocama <tinoclase@gmail.com>
2022-05-18 12:36:39 +0200

    descenso a la locura (parte 1)

--- noticiaIndividual.php
+++ noticiaIndividual.php
@@ -47,10 +47,9 @@ $security->register();
             </div>
         </aside>
         <section class="noticia">
-            <div class="titulo" id="titulo"><br>
-                <cite class="autor">Escrito por <a href="#" class="users"></a></cite>
+            <div class="titulo" id="titulo"></div><br>
+                <cite class="autor">Escrito por <a href="#" class="users" id="user"></a></cite>
                 <div class="dia" id="dia"> 9 Mayo 2022</div>
-                <div id="usuario"></div>
             </div>
             <div class="plantilla0" id="plantilla0">
                 <h1 class="titulo1" id="titulo1"></h1>
