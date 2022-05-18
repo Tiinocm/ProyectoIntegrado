4faf096392ca2ce67b6235ed0e4e2b845554db40
AlcosaC <alcosa@floridauniversitaria.es>
2022-05-18 09:14:08 +0200

    prueba like

--- likes.js
+++ likes.js
@@ -4,18 +4,22 @@ document.addEventListener("DOMContentLoaded", main)
 
 function main()
 {
-
-    const xhttp = new XMLHttpRequest();
+     let icon = document.getElementById("icon")
+     icon.addEventListener("click",function(){
+         icon.classList.replace("fa-regular","fa-solid");
+         
+    /*     const xhttp = new XMLHttpRequest();
     xhttp.addEventListener("readystatechange", function(){
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.responseText);
             console.log(data);
-            console.log(data[0]["usuario"]);
-            document.getElementById("dia").innerHTML = data[0]["fecha"];
-            document.getElementById("titulo").innerHTML = data[0]["titulo"];
+            
 
         }
     });
-    xhttp.open("GET", "likes.php" + get, true);
-    xhttp.send();
+    xhttp.open("GET", "likes.php", true);
+    xhttp.send(); */
+
+     });
+    
 }
