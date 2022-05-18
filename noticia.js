beaaaf2799acec029e422dcea112da7901e5f6e0
ALUMNADO\cocama <tinoclase@gmail.com>
2022-05-18 10:57:36 +0200

    prueba parrafo1

--- noticia.js
+++ noticia.js
@@ -8,13 +8,14 @@ function main()
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.responseText);
             console.log(data);
-            console.log(data[0]["user"]);
+            console.log(data[0]["usuario"]);
             document.getElementById("dia").innerHTML = data[0]["fecha"];
             document.getElementById("titulo").innerHTML = data[0]["titulo"];
             /* document.getElementById("user").innerHTML = "alfajor"; */
             if (data[0]["plantilla"] == 0) {
                 document.getElementById("plantilla0").style.display = "block";
                 document.getElementById("titulo1").innerHTML = data[0]["titulo1"];
+                document.getElementById("parrafo1").innerHTML = data[0]["parrafo1"];
                 
             }else{
                 document.getElementById("plantilla1").style.display = "block";
