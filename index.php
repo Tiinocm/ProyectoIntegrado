46422d68f1028eda3934645b0cb3edd94bda9509
ALUMNADO\cocama <tinoclase@gmail.com>
2022-05-20 10:12:57 +0200

    quitado display de errores

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 require_once "autoloading.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $cronos = new cronos();
 $cronos->getNoticias(1);
 
