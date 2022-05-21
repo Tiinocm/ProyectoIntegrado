260e573ecc5898159c06f9a42cac659841c52a42
PC-TINO\Tino <tinoclase@gmail.com>
2022-05-21 19:26:42 +0200

    mejor renombre de ficheros js

--- noticia.js
+++ noticia.js
@@ -20,8 +20,10 @@ function main()
                 document.getElementById("titulo2").innerHTML = data[0]["titulo2"];
                 document.getElementById("parrafo1").innerHTML = data[0]["parrafo1"];
                 document.getElementById("parrafo2").innerHTML = data[0]["parrafo2"];
-                document.getElementById("img1").src = data[0]["img1"].slice(1, data[0]["imagen_portada"].length -2);
-                console.log(data[0]["img1"].slice(1, data[0]["imagen_portada"].length -3));
+                let img1 = data[0]["img1"].slice(1, data[0]["imagen_portada"].length -2);
+                img1 = img1.split("\"");
+                img1 = img1[0];
+                document.getElementById("img1").src = img1;
                 document.getElementById("img2").src = data[0]["img2"].slice(1, data[0]["imagen_portada"].length -2);
             }else{
                 document.getElementById("plantilla1").style.display = "block";
